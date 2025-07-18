945971a06a9648b255665a2aa035de22ff89a254
TamannaShrestha-19 <tmnnshrsth@gmail.com>
2025-07-18 14:44:51 +0545

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Smooth scrolling for nav links
+document.querySelectorAll('a.nav-link').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+    });
+});
